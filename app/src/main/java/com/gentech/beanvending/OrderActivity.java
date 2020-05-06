38061301544fd838c248cf857a303bb8d2840056
Somsak Binarwaeloh <bsomsak.dev@gmail.com>
2020-05-06 13:45:41 +0700

    add feature load username on OrderAcitivity

--- app/src/main/java/com/gentech/beanvending/OrderActivity.java
+++ app/src/main/java/com/gentech/beanvending/OrderActivity.java
@@ -2,13 +2,41 @@ package com.gentech.beanvending;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+
+    private static final String SHARED_PREFS = "sharedPrefs";
+    private static final String usernameKey = "username";
+    private static String usernameValue = "";
+    private String username;
+    private Context context;
+
+
+    private TextView tvUsername;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        context = getApplicationContext();
+        username = loadPreferences(usernameKey);
+
+        tvUsername = (TextView) findViewById(R.id.tvUsername);
+        tvUsername.setText(username);
+
+    }
+
+
+    public String loadPreferences(String key) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        return sharedPreferences.getString(key, "") ;
     }
 }
